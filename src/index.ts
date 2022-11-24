8815cc4a782ab9dd7731721aaed5960ceea5e478
Lucie Brackenier <lucie.brackenier@ynov.com>
2022-11-24 11:36:13 +0100

    port changed to 8000

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const debug = d('express-generator:server')
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '8000');
 app.set('port', port);
 
 /**
