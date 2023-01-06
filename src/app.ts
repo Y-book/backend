8d462d40d87066b9fb6ff38598ad4058d46d65dc
Lucie Brackenier <lucie.brackenier@ynov.com>
2023-01-06 08:53:59 +0100

    removed duplicated dependancies

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
-import { Response, Request, NextFunction, Router } from "express"
-
+import express, { Response, Request, NextFunction } from "express"
 import createError from "http-errors"
-import express from "express"
 import path from "path"
 import cookieParser from "cookie-parser"
 import logger from "morgan";
