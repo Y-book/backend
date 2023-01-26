a78bd651e8a8afd966de92df05c75ce9b4aab574
Lucie Brackenier <lucie.brackenier@ynov.com>
2023-01-26 08:48:17 +0100

    added user research with a connected account

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -6,9 +6,9 @@ const UserRouter = express.Router()
 
 UserRouter
     .post("/", userController.createUser)
-    .get("/search-users", userController.getUsersWithResearch)
     .use(verifyCognito)
     .get("/all", userController.getUsers)
+    .get("/search-users", userController.getUsersWithResearch)
     .get("/", userController.getConnectedUserById)
     .get("/:id", userController.getUserById)
     .patch("/:id", userController.updateUser)
